cfc7c053899d6550e73e328f811458826ef29ab7
Maxine <maxinehartnett@gmail.com>
2018-02-24 16:48:08 -0700

    First commit

---
+++ blockchain.py
@@ -0,0 +1,7 @@
+def Main(method, user_pk, business_pk, charity_pk, amount, txid):
+    if method == 1:
+        # Make structure containing info
+    if method == 2:
+        # Make method to update structure based on business Neo adding
+    if method == 3:
+        # Charity gets Neo from completed contracts
---
+++ compiler.py
@@ -0,0 +1,4 @@
+import hello_world
+from boa.compiler import Compiler
+
+Compiler.load_and_save('hello_world.py')
